171211b83fa97210ef170c39b982fcc737b035aa
Kevin Chen <kchen01@stuy.edu>
2018-10-03 18:01:15 -0400

    Finished with creating both things and some methods

---
+++ BankAccount.java
@@ -0,0 +1,23 @@
+public class BankAccount {
+  private double balance;
+  private int accountID;
+  private String password;
+
+  public BankAccount(double b,int a, String p) {
+    balance = b;
+    accountID = a;
+    password = p;
+  }
+  public double getBalance() {
+    return balance;
+  }
+  public int getAccountID() {
+    return accountID;
+  }
+  public void setPassword(String newPassword) {
+    password = newPassword;
+  }
+  public String toString() {
+    return accountID + "      " + balance;
+  }
+}
---
+++ Driver.java
@@ -0,0 +1,7 @@
+public main Driver {
+  //new BankAccount using constructor
+  BankAccount a = BankAccount(125.00,123,"abc123");
+
+  System.out.println(a)
+  // should print 123     125.00 
+}
